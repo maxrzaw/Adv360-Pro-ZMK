873422ac596819956d1ee285b8df5546581d489b
Max Zawisa <maxrzaw@umich.edu>
2026-05-08 21:57:09 -0400

    turn off usb logging by default

--- bin/build.sh
+++ bin/build.sh
@@ -5,9 +5,15 @@ set -eu
 PWD=$(pwd)
 TIMESTAMP="${TIMESTAMP:-$(date -u +"%Y%m%d%H%M")}"
 COMMIT="${COMMIT:-$(echo xxxxxx)}"
+USB_LOGGING="${USB_LOGGING:-false}"
+
+USB_LOGGING_SNIPPET=()
+if [ "${USB_LOGGING}" = true ]; then
+    USB_LOGGING_SNIPPET=(-S zmk-usb-logging)
+fi
 
 # West Build (left)
-west build -s zmk/app -p -d build/left -b adv360_left -S studio-rpc-usb-uart -S zmk-usb-logging -- -DZMK_CONFIG="${PWD}/config" -DCONFIG_ZMK_STUDIO=y
+west build -s zmk/app -p -d build/left -b adv360_left -S studio-rpc-usb-uart "${USB_LOGGING_SNIPPET[@]}" -- -DZMK_CONFIG="${PWD}/config" -DCONFIG_ZMK_STUDIO=y
 # Adv360 Left Kconfig file
 grep -vE '(^#|^$)' build/left/zephyr/.config
 # Rename zmk.uf2
@@ -16,7 +22,7 @@ cp build/left/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}-${COMMIT}-left-clique.uf2"
 # Build right side if selected
 if [ "${BUILD_RIGHT}" = true ]; then
     # West Build (right)
-    west build -s zmk/app -p -d build/right -b adv360_right -S zmk-usb-logging -- -DZMK_CONFIG="${PWD}/config"
+    west build -s zmk/app -p -d build/right -b adv360_right "${USB_LOGGING_SNIPPET[@]}" -- -DZMK_CONFIG="${PWD}/config"
     # Adv360 Right Kconfig file
     grep -vE '(^#|^$)' build/right/zephyr/.config
     # Rename zmk.uf2
